c8cc75ad78d5fa4e3abca8b4003c9bf739553719
MattGuy2 <matthewfrankflammia@gmail.com>
2021-08-08 15:49:18 -0400

    adding update code to check on all messages

--- ErikoBot.js
+++ ErikoBot.js
@@ -39,6 +39,65 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', async message => {
+	
+	let currentTime = new Date();	// this will update every time there is a message emitted, essentially working as a time of message
+	
+	if(currentTime.getUTCHours() < 13 && !dayHasPassed){
+		//if the current time does not equal 13 UTC (9am EST) then it will reset the daily reset checker to false
+		//this is to prevent the issue where if a person was triggering the bot during 13 UTC it would be constantly resetting
+		//the database, therefore clearing out an entire hours worth of work
+		console.log('setting day has passed to true in time check');
+		dayHasPassed = true;
+	}
+	
+	//triggers at 9AM EST or 13 UTC
+	if(currentTime.getUTCHours() >= 13 && dayHasPassed){
+		if(!fs.existsSync(`./config.json`)){
+			console.log('No config file found');
+		}
+		else{
+			let configRead = fs.readFileSync(`./config.json`);
+			let configJSON = JSON.parse(configRead);
+			for(let cfg=0;cfg<configJSON.servers.length;cfg++){
+				if(!fs.existsSync(`./databases/${configJSON.servers[cfg].startCB}${message.guild.id}${configJSON.servers[cfg].endCB}.json`)){
+					//if there is no database file, do nothing
+					console.log('No database file found');
+				}
+				else{
+					//this sets the daily reset flag to true, meaning that the daily reset has already occurred, see above comment for
+					//more information on why this is done
+					console.log('setting day has passed to false in daily recording and reset')
+					dayHasPassed = false;
+					//database read in local directory and parsing it into JSON object
+					let dataRead = fs.readFileSync(`./databases/${configJSON.servers[cfg].startCB}${message.guild.id}${configJSON.servers[cfg].endCB}.json`);
+					let dataJSON = JSON.parse(dataRead);
+					//loop that iterates over all users and resets their daily hits to 0
+					for(let i=0;i<dataJSON.users.length;i++){
+						console.log('updating hits for ' + dataJSON.users[i].name);
+						//setting yesterdays hits
+						currentTime.setDate(currentTime.getDate() - 1);
+						//time variables that pad zeros and un-zero index the month
+						let recordMonth = ('0' + (currentTime.getUTCMonth()+1)).slice(-2);
+						let recordDay = ('0' + currentTime.getUTCDate()).slice(-2);
+						//the date formatted as MMDDYYYY
+						let formattedDate = "" + recordMonth + recordDay + currentTime.getUTCFullYear();
+						let hits = dataJSON.users[i].hits
+						console.log('hits for ' + formattedDate + ' ' + hits);
+						//create the input for the user
+						let newDailyHits = {'date':formattedDate,'hits':hits};
+						dataJSON.users[i].total.push(newDailyHits);
+						
+						//actual setting of each user to 0
+						dataJSON.users[i].hits = 0;
+					}
+					//JSON compression and sync file write to database file, overwriting it
+					let dataSave = JSON.stringify(dataJSON);
+					fs.writeFileSync(`./databases/${configJSON.servers[cfg].startCB}${message.guild.id}${configJSON.servers[cfg].endCB}.json`,dataSave);
+				}
+			}
+		}
+	}
+	
 	if (message.content.toLowerCase() === '!eriko deploy' && message.author.id == '492850107038040095') {
 		console.log('deploying commands');
 		const data = [
