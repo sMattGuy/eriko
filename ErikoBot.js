ea9c5de729d244ea1a7367dd87471c30b6151638
MattGuy2 <matthewfrankflammia@gmail.com>
2021-08-31 10:32:07 -0400

    bug fix

--- ErikoBot.js
+++ ErikoBot.js
@@ -196,7 +196,7 @@ client.on('interactionCreate', async interaction => {
 		await client.commands.get(interaction.commandName).execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
