e49129ea144213f2c33a7e47bd6700d2e6a125ce
MattGuy2 <matthewfrankflammia@gmail.com>
2021-12-29 13:44:32 -0500

    await defer

--- commands/user/checkHits.js
+++ commands/user/checkHits.js
@@ -33,7 +33,6 @@ module.exports = {
 				}
 				else{
 					let channel = interaction.channel;
-					await interaction.deferReply();
 					//read the database
 					let dataRead = fs.readFileSync(`./databases/${configJSON.servers[cfg].startCB}${interaction.guild.id}${configJSON.servers[cfg].endCB}.json`);
 					let dataJSON = JSON.parse(dataRead);
