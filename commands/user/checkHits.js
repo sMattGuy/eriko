a214cba20257e1a8203cc56d80555f7e8a182446
Mico <miyoumu@gmail.com>
2022-09-30 10:35:56 -0400

    fixed issue with checkhits not working like it did before database migration

--- commands/user/checkHits.js
+++ commands/user/checkHits.js
@@ -21,24 +21,74 @@ module.exports = {
 		let userData = "";
 		if(selectedDate.length != 8){
 			//not a date
-			cbConfig = await cb.findOne({where:{'server_id': interaction.guild.id, 'cb_id':parseInt(selectedDate)}});
-			//get entire cb results
-			userData = await todayshits.getAllHits(parseInt(selectedDate), interaction.guild.id);
+			//solve for date of cb
+			cbConfig = await cb.getRecentServerConfig(interaction.guild.id);
+			if(!cbConfig){
+				console.log('No cb found');
+				const noConfigEmbed = new EmbedBuilder()
+					.setColor('#E3443B')
+					.setDescription(`No CB data was found for this server!`);
+				await interaction.editReply({embeds:[noConfigEmbed]});
+				return;
+			}
+			if(parseInt(selectedDate) <= 0){
+				console.log('Invalid day');
+				const noConfigEmbed = new EmbedBuilder()
+					.setColor('#E3443B')
+					.setDescription(`Invalid day entered!`);
+				await interaction.editReply({embeds:[noConfigEmbed]});
+				return;
+			}
+			let savedTime = cbConfig.start_date;
+			let savedEndTime = cbConfig.end_date;
+			let startCB = new Date(`${savedTime.substring(0,2)}/${savedTime.substring(2,4)}/${savedTime.substring(4,8)}`);
+			let endCB = new Date(`${savedEndTime.substring(0,2)}/${savedEndTime.substring(2,4)}/${savedEndTime.substring(4,8)}`);
+
+			startCB.setUTCHours(13);
+			startCB.setUTCMinutes(0);
+			startCB.setUTCSeconds(0);
+			startCB.setUTCMilliseconds(0);
+			
+			endCB.setUTCHours(13);
+			endCB.setUTCMinutes(0);
+			endCB.setUTCSeconds(0);
+			endCB.setUTCMilliseconds(0);
+
+			let lookDate = new Date();
+			lookDate.setUTCDate(startCB.getUTCDate() + parseInt(selectedDate) - 1);
+			if(lookDate > endCB){
+				console.log('Invalid day');
+				const noConfigEmbed = new EmbedBuilder()
+					.setColor('#E3443B')
+					.setDescription(`There is no data after the CB ended!`);
+				await interaction.editReply({embeds:[noConfigEmbed]});
+				return;
+			}
+			let currentDate = new Date();
+			currentDate.setUTCHours(13);
+			currentDate.setUTCMinutes(0);
+			currentDate.setUTCSeconds(0);
+			currentDate.setUTCMilliseconds(0);
+
+			if(currentDate == lookDate){
+				console.log('Invalid day');
+				const noConfigEmbed = new EmbedBuilder()
+					.setColor('#E3443B')
+					.setDescription(`Use /checktodayshits!`);
+				await interaction.editReply({embeds:[noConfigEmbed]});
+				return;
+			}
+			let finalMonth = ('0'+(lookDate.getUTCMonth()+1)).slice(-2);
+			let finalDay = ('0'+(lookDate.getUTCDate())).slice(-2);
+			let finalDate = ""+finalMonth+finalDay+lookDate.getUTCFullYear();
+			console.log(`final date: ${finalDate}`);
+			//search for hits from that day
+			userData = await todayshits.findAll({where:{'server_id': interaction.guild.id, 'date': finalDate}});
 		}
 		else{
 			//date
-			cbConfig = await cb.findOne({where:{'server_id': interaction.guild.id, 'start_date':selectedDate}});
 			userData = await todayshits.findAll({where:{'server_id': interaction.guild.id, 'date': selectedDate}})
-		}
-
-		if(!cbConfig){
-			console.log('No config file found');
-			const noConfigEmbed = new EmbedBuilder()
-				.setColor('#E3443B')
-				.setDescription(`The next CB has not been set up!`);
-			interaction.editReply({embeds:[noConfigEmbed]});
-			return;
-		}
+		}	
 		if(userData.length == 0){
 			console.log('No user data found');
 			const noConfigEmbed = new EmbedBuilder()
@@ -52,8 +102,11 @@ module.exports = {
 		//initialize message so discord js doesn't crash
 		for(let i=0;i<userData.length;i++){
 			let usersHits = userData[i].n_hits;
+			if(!usersHits){
+				usersHits = userData[i].hits;
+			}
 			totalHits += usersHits;
-			let userNick = await interaction.guild.members.fetch(userData[i].user_id).then(user => {return user.displayName}).catch(e => {return 'NoNameError'});
+			let userNick = await interaction.guild.members.fetch(userData[i].user_id).then(user => {return user.displayName}).catch(e => {return `${userData[i].user_id}`});
 			let userObject = {name:userNick,hits:usersHits};
 			userArray.push(userObject);
 		}
@@ -61,7 +114,7 @@ module.exports = {
 			return parseInt(b.hits) - parseInt(a.hits);
 		});
 		
-		interaction.editReply(`Total  hits for ${selectedDate}\n`);
+		await interaction.editReply(`Total  hits for ${selectedDate}\n`);
 		let messageToSend = '';
 		for(let i=0;i<userArray.length;i++){
 			let messageAddition = `${userArray[i].name} : ${userArray[i].hits}\n`;
